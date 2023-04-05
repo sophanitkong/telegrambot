52d7d63bb2ad62d8b6ee53fe999878d0f08af517
Kong Sophanit <38678876+sophanitkong@users.noreply.github.com>
2023-04-05 10:51:14 +0700

    Initialize Project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require('node-telegram-bot-api');
+require("dotenv").config();
+
+const TOKEN = process.env.TOKEN;
+
+const bot = new TelegramBot(TOKEN,{polling: true})
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "👋 Hello! This is CanaMerchant Bot by Canadia Bank."+"\n"+
+    "Please enter the 4 digits code given in the previous screen and send.");
+    });
+
+    bot.onText(/\/showmyid/, (msg) => {
+        bot.sendMessage(msg.chat.id, `Your Telegram Group ID: \`${msg.chat.id}\``);
+        });
+
+        bot.on("message",(msg)=>{
+            if(msg.text.toString().toLowerCase() == "1234")
+            bot.sendMessage(msg.chat.id, `Your Telegram Group ID: \`${msg.chat.id}\``);
+        });
+        
