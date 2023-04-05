fd37b4cc391e39a5248f8fc4bb450c9433fed6ed
Kong Sophanit <38678876+sophanitkong@users.noreply.github.com>
2023-04-05 14:05:12 +0700

    Clean up some code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
 const TOKEN = process.env.TOKEN;
 
-const bot = new TelegramBot(TOKEN,{polling: true})
+const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "👋 Hello! This is CanaMerchant Bot by Canadia Bank."+"\n"+
-    "Please enter the 4 digits code given in the previous screen and send.");
-    });
+  bot.sendMessage(
+    msg.chat.id,
+    "👋 Hello! This is CanaMerchant Bot by Canadia Bank." +
+      "\n" +
+      "Please enter the 4 digits code given in the previous screen and send."
+  );
+});
 
-    bot.onText(/\/showmyid/, (msg) => {
-        bot.sendMessage(msg.chat.id, `Your Telegram Group ID: \`${msg.chat.id}\``);
-        });
+bot.onText(/\/showmyid/, (msg) => {
+  bot.sendMessage(msg.chat.id, `Your Telegram Group ID: \`${msg.chat.id}\``);
+});
 
-        bot.on("message",(msg)=>{
-            if(msg.text.toString().toLowerCase() == "0000")
-            bot.sendMessage(msg.chat.id, `🎉 Congratulations! You’ve successfully linked KONG SOPHANIT to this Telegram group. All Canadia Pay payment notifications will appear here. Thank you for choosing Canadia Bank.`);
-        });
-        
+bot.on("message", (msg) => {
+  if (msg.text.toString().toLowerCase() == "0000")
+    bot.sendMessage(
+      msg.chat.id,
+      `🎉 Congratulations! You’ve successfully linked KONG SOPHANIT to this Telegram group. All Canadia Pay payment notifications will appear here. Thank you for choosing Canadia Bank.`
+    );
+});
